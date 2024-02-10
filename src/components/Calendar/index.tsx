d0b05097af5681d6080837a1994b58dbaf9e52e0
Plaginnnn <mulyukov.rasim@mail.ru>
2024-02-10 03:19:34 +0300

    deploy

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Calendar from 'react-calendar'
+import Calendar, { CalendarProps } from 'react-calendar'
 import 'react-calendar/dist/Calendar.css'
 import './customCalendar.css'
 
@@ -7,7 +7,7 @@ interface ScheduleDate {
 	date: string
 }
 
-const CalendarMain = () => {
+const CalendarMain: React.FC = () => {
 	const [value, onChange] = useState<Date>(new Date())
 	const [selectedDate, setSelectedDate] = useState<Date | null>(null)
 	const [showNavigation, setShowNavigation] = useState<boolean>(false)
@@ -24,18 +24,23 @@ const CalendarMain = () => {
 		onChange(nextMonth)
 	}
 
-	const handleCalendarChange = (value: any) => {
-		onChange(value)
-		setSelectedDate(value)
-
-		const formattedDate: ScheduleDate = formatDate(value)
-		console.log(formattedDate)
+	const handleCalendarChange: CalendarProps['onChange'] = (value, event) => {
+		if (value instanceof Date) {
+			onChange(value)
+			setSelectedDate(value)
+			event
+			formatDate(value)
+		} else {
+			console.error('Unexpected value type in handleCalendarChange')
+		}
 	}
 
 	const formatDate = (date: Date): ScheduleDate => {
 		const year = date.getFullYear()
 		const month = (date.getMonth() + 1).toString().padStart(2, '0')
 		const day = date.getDate().toString().padStart(2, '0')
+		console.log({ date: `${year}-${month}-${day}` })
+		// Пример того что мы получили {date: '2024-02-08'}
 		return { date: `${year}-${month}-${day}` }
 	}
 
